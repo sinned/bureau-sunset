bcb1ed17e143f0bc9b1b081736f33cc2d1955576
Dennis Yang <sinned@gmail.com>
2014-08-28 09:18:08 -0700

    lots of content blocks

--- index.php
+++ index.php
@@ -40,7 +40,7 @@
   <div data-0="transform:translate(0,100%);" data-100p="transform:translate(0,0%)" data-_box-100p="" data-_box-200p="transform:translate(0,-100%)">
     <div id="secondblock">
 
-      <div style="position:absolute;margin-top:-2000px;margin-left:-50px;width:100%;text-align:center;" data-130p="margin-top:-2000px;" data-150p="margin-top:0px;" data-190p="margin-top:0px;opacity:1;" data-200p="margin-top:-2000px;opacity:0;"><h2>Thank <br /> You.</h2></div>
+      <div style="position:absolute;margin-top:-2000px;margin-left:-50px;width:100%;text-align:center;" data-130p="margin-top:-2000px;" data-150p="margin-top:0px;" data-190p="margin-top:0px;opacity:1;" data-500p="margin-top:-2000px;opacity:0;"><h2>Thank <br /> You.</h2></div>
 
       <div style="float:left;margin:30px 50px 0 0;" data-100p="opacity:0;" data-110p="opacity:1;" data-180p="margin-top:30px;opacity:1;" data-200p="margin-top:-1000px;opacity:0;">It's our time to go. <br />Nothing to miss.</div>
       <div style="float:right;text-align:right;margin: 30px 0 0 0;" data-110p="opacity:0;" data-120p="opacity:1;" data-180p="margin-top:30px;opacity:1;" data-200p="margin-top:-1000px;opacity:0;">Nothing to mourn. <br /> Only this to say:</div>
@@ -48,11 +48,38 @@
   </div>
 
   <div data-200p="transform:translate(0,100%);" data-300p="transform:translate(0,0%)" data-_box-300p="" data-_box-400p="transform:translate(0,-100%)">
-    <div id="thirdblock">
+    <div id="thirdblock" data-390p="opacity:1;" data-400p="opacity:0;">
       <img src="img/clock.gif" alt="Bureau Clock"/>
       <div>To each of you that ever spent a fleeting moment with us.</div>
+      <div style="margin-top:20px;" data-340p="opacity:0;font-size:30px;" data-370p="opacity:1;font-size:40px;"><h2>Moments Matter.</h2></div>
     </div>
   </div>  
+
+  <div data-400p="transform:translate(0,100%);" data-500p="transform:translate(0,0%)" data-_box-500p="" data-_box-600p="transform:translate(0,-100%)">
+    <div class="contentblock" data-590p="opacity:1;" data-600p="opacity:0;">
+      <img src="img/star.png" alt="Bureau Star" style="width:400px" />
+      <div>To each of you that made mention of our <br /> tiny little flickering light in the night.</div>
+      <div style="margin-top:20px;" data-540p="opacity:0;font-size:30px;" data-570p="opacity:1;font-size:40px;"><h2>Mentions Matter.</h2></div>
+    </div>
+  </div>    
+
+  <div data-600p="transform:translate(0,100%);" data-700p="transform:translate(0,0%)" data-_box-700p="" data-_box-800p="transform:translate(0,-100%)">
+    <div class="contentblock" data-790p="opacity:1;" data-800p="opacity:0;">
+      <img src="img/suitable-notsuitable.png" alt="Bureau S/NS" style="width:400px" />
+      <div>To each of you that ever smirked, cackled, or laughed <br/> at our sophomoric ranklings</div>
+      <div style="margin-top:20px;" data-740p="opacity:0;font-size:30px;" data-770p="opacity:1;font-size:40px;"><h2>Laughter Matters.</h2></div>
+    </div>
+  </div>      
+
+  <div data-800p="transform:translate(0,100%);" data-900p="transform:translate(0,0%)" data-_box-900p="" data-_box-1000p="transform:translate(0,-100%)">
+    <div class="contentblock" data-990p="opacity:1;" data-1000p="opacity:0;">
+      <div style="margin: 0 auto;background:url('http://media.bureauoftrade.com/img/category-icons@2x.png') top left; width: 800px; height: 70px;"></div>
+      <div style="margin: 0 auto 20px;background:url('http://media.bureauoftrade.com/img/category-icons@2x.png') top left; width: 700px; height: 70px; background-position: -800px 0;"></div>      
+      <br />
+      <div>To each of you that voted with your feet and made a <br />purchase &mdash; not a thing to buy, but a good to cherish</div>
+      <div style="margin-top:20px;" data-940p="opacity:0;font-size:30px;" data-970p="opacity:1;font-size:40px;"><h2>Goods Matter.</h2></div>
+    </div>
+  </div>       
 
   <script type="text/javascript" src="js/skrollr.min.js"></script>
 
