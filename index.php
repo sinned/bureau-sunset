1c1814b2b1104b09cace2f35e1b5c475db76ed14
Dennis Yang <sinned@gmail.com>
2014-08-27 21:48:19 -0700

    good starting commit

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+
+  <title>Bureau of Trade</title>
+  <link rel="stylesheet" href="sunset.css" type="text/css">
+  <link rel="stylesheet" href="fonts.css" type="text/css">
+
+  <script src="//www.google.com/jsapi"></script>
+  <script src="//ajax.googleapis.com/ajax/libs/swfobject/2.1/swfobject.js"></script>
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+  <div id="youtubeNosound"></div>
+  <script type="text/javascript">
+  /*
+    google.load("swfobject", "2.1");
+    function onYouTubePlayerReady(playerId) {
+      bagman_vid = document.getElementById("bagman_vid");
+      bagman_vid.playVideo();
+      bagman_vid.mute();
+    }
+
+    // bagman vid
+    var params = { allowScriptAccess: "always" };
+    var atts = { id: "bagman_vid" };
+    swfobject.embedSWF("http://www.youtube.com/v/byB8g5YEgIw?start=15&end=27&rel=0&enablejsapi=1&playerapiid=bagman_vid&allowFullScreen=true&version=3&loop=1&showinfo=0&autohide=1&hd=1&controls=0&autoplay=1","bagman_vid", "853", "448", "8", null, null, params, atts)
+  */
+  </script>  
+</head>
+
+<body>
+  <div id="firstblock" data-0="transform:translate(0,0%);" data-100p="transform:translate(0,-100%);">
+    <div id="nostalgic-overlay" data-0="opacity:1;" data-100p="opacity:0;" >
+      <span id="nostalgic-text"><h2>Be Nostalgic</h2><br /><h2>For the Future</h2></span>
+    </div>
+  </div>
+
+  <div data-0="transform:translate(0,100%);" data-100p="transform:translate(0,0%)" data-_box-100p="" data-_box-200p="transform:translate(0,-100%)">
+    <div id="secondblock">
+
+      <div style="position:absolute;margin-top:-2000px;margin-left:-50px;width:100%;text-align:center;" data-130p="margin-top:-2000px;" data-150p="margin-top:0px;" data-190p="margin-top:0px;opacity:1;" data-200p="margin-top:-2000px;opacity:0;"><h2>Thank <br /> You.</h2></div>
+
+      <div style="float:left;margin:30px 50px 0 0;" data-100p="opacity:0;" data-110p="opacity:1;" data-180p="margin-top:30px;opacity:1;" data-200p="margin-top:-1000px;opacity:0;">It's our time to go. <br />Nothing to miss.</div>
+      <div style="float:right;text-align:right;margin: 30px 0 0 0;" data-110p="opacity:0;" data-120p="opacity:1;" data-180p="margin-top:30px;opacity:1;" data-200p="margin-top:-1000px;opacity:0;">Nothing to mourn. <br /> Only this to say:</div>
+    </div>
+  </div>
+
+  <div data-200p="transform:translate(0,100%);" data-300p="transform:translate(0,0%)" data-_box-300p="" data-_box-400p="transform:translate(0,-100%)">
+    <div id="thirdblock">
+      <img src="img/clock.gif" alt="Bureau Clock"/>
+      <div>To each of you that ever spent a fleeting moment with us.</div>
+    </div>
+  </div>  
+
+  <script type="text/javascript" src="js/skrollr.min.js"></script>
+
+  <script type="text/javascript">
+  skrollr.init({
+    constants: {
+      //fill the box for a "duration" of 150% of the viewport (pause for 150%)
+      //adjust for shorter/longer pause
+      box: '150p'
+    }
+  });
+  </script>
+  
+</body>
+</html>
