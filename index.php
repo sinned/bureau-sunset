3fc51d6fa767a785c60d06b1ff5af6bb2e4a87aa
Dennis Yang <sinned@gmail.com>
2014-09-04 13:03:24 -0700

    all content in

--- index.php
+++ index.php
@@ -39,9 +39,7 @@
 
   <div data-0="transform:translate(0,100%);" data-100p="transform:translate(0,0%)" data-_box-100p="" data-_box-200p="transform:translate(0,-100%)">
     <div id="secondblock">
-
       <div style="position:absolute;margin-top:-2000px;margin-left:-50px;width:100%;text-align:center;" data-130p="margin-top:-2000px;" data-150p="margin-top:0px;" data-190p="margin-top:0px;opacity:1;" data-500p="margin-top:-2000px;opacity:0;"><h2>Thank <br /> You.</h2></div>
-
       <div style="float:left;margin:30px 50px 0 0;" data-100p="opacity:0;" data-110p="opacity:1;" data-180p="margin-top:30px;opacity:1;" data-200p="margin-top:-1000px;opacity:0;">It's our time to go. <br />Nothing to miss.</div>
       <div style="float:right;text-align:right;margin: 30px 0 0 0;" data-110p="opacity:0;" data-120p="opacity:1;" data-180p="margin-top:30px;opacity:1;" data-200p="margin-top:-1000px;opacity:0;">Nothing to mourn. <br /> Only this to say:</div>
     </div>
@@ -56,7 +54,7 @@
   </div>  
 
   <div data-400p="transform:translate(0,100%);" data-500p="transform:translate(0,0%)" data-_box-500p="" data-_box-600p="transform:translate(0,-100%)">
-    <div class="contentblock" data-590p="opacity:1;" data-600p="opacity:0;">
+    <div class="contentblock centervert" data-590p="opacity:1;" data-600p="opacity:0;">
       <img src="img/star.png" alt="Bureau Star" style="width:400px" />
       <div>To each of you that made mention of our <br /> tiny little flickering light in the night.</div>
       <div style="margin-top:20px;" data-540p="opacity:0;font-size:30px;" data-570p="opacity:1;font-size:40px;"><h2>Mentions Matter.</h2></div>
@@ -64,7 +62,7 @@
   </div>    
 
   <div data-600p="transform:translate(0,100%);" data-700p="transform:translate(0,0%)" data-_box-700p="" data-_box-800p="transform:translate(0,-100%)">
-    <div class="contentblock" data-790p="opacity:1;" data-800p="opacity:0;">
+    <div class="contentblock centervert" data-790p="opacity:1;" data-800p="opacity:0;">
       <img src="img/suitable-notsuitable.png" alt="Bureau S/NS" style="width:400px" />
       <div>To each of you that ever smirked, cackled, or laughed <br/> at our sophomoric ranklings</div>
       <div style="margin-top:20px;" data-740p="opacity:0;font-size:30px;" data-770p="opacity:1;font-size:40px;"><h2>Laughter Matters.</h2></div>
@@ -72,7 +70,7 @@
   </div>      
 
   <div data-800p="transform:translate(0,100%);" data-900p="transform:translate(0,0%)" data-_box-900p="" data-_box-1000p="transform:translate(0,-100%)">
-    <div class="contentblock" data-990p="opacity:1;" data-1000p="opacity:0;">
+    <div class="contentblock centervert">
       <div style="margin: 0 auto;background:url('http://media.bureauoftrade.com/img/category-icons@2x.png') top left; width: 800px; height: 70px;"></div>
       <div style="margin: 0 auto 20px;background:url('http://media.bureauoftrade.com/img/category-icons@2x.png') top left; width: 700px; height: 70px; background-position: -800px 0;"></div>      
       <br />
@@ -80,6 +78,34 @@
       <div style="margin-top:20px;" data-940p="opacity:0;font-size:30px;" data-970p="opacity:1;font-size:40px;"><h2>Goods Matter.</h2></div>
     </div>
   </div>       
+
+  <div data-900p="transform:translate(0,100%);" data-1000p="transform:translate(0,0%)" data-_box-1200p="" data-_box-1300p="transform:translate(0,-100%)">
+    <div class="contentblock" data-1000p="width:0%;" data-_box-1100p="width:100%;" style="padding:0;position:absolute;left:0px;height:100%;background:#09f;">
+      <h2 style="margin-top:350px;" data-1250p="opacity:0;" data-1300p="opacity:1;">Belief Matters.</h2>
+    </div>
+  </div>
+
+  <div data-1300p="transform:translate(0,100%);" data-1400p="transform:translate(0,0%)" data-_box-1400p="" data-_box-1500p="transform:translate(0,-100%)">
+    <div class="contentblock centervert">
+      <div>To our team of mercenaries, yeomen, and artists <br /> who toiled in the trenches with us</div>
+      <div style="margin-top:40px;" data-1460p="opacity:0;font-size:1px;" data-1500p="opacity:1;font-size:60px;"><h2>Community Matters.</h2></div>
+    </div>
+  </div>     
+
+  <div data-1500p="transform:translate(0,100%);" data-1600p="transform:translate(0,0%)" data-_box-1600p="" data-_box-1700p="transform:translate(0,-100%)">
+    <div class="contentblock centervert">
+      <h2>With</h2>
+      <h2>gratitude,</h2>
+      <h2>humility,</h2>
+      <h2>and love.</h2>
+    </div>
+  </div>
+
+  <div data-1700p="transform:translate(0,100%);" data-1800p="transform:translate(0,0%)">
+    <div class="contentblock centervert">
+      <h2>BUREAU OF TRADE</h2>
+    </div>
+  </div>  
 
   <script type="text/javascript" src="js/skrollr.min.js"></script>
 
